9c46ed6d368061cf5bddcb60d94d9bb66d9e5842
B-MEbrahim <mahmoudebr24@gmail.com>
2026-02-11 20:57:32 +0200

    base models

--- src/models.py
+++ src/models.py
@@ -1,3 +1,9 @@
 from pydantic import BaseModel, Field
 from typing import Any, Dict, Union, Optional
 
+class Regulations(BaseModel):
+    year: int = Field(2026, description="Year of FIA Regulations")
+
+class Race(BaseModel):
+    year: int = Field(2026, description="Race Year.")
+    gp_name: str = Field(..., description="Name of Country where Race is held.")
