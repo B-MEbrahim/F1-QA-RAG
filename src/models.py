86e7da407b071e34b8436652ff09f748a50d41d8
B-MEbrahim <mahmoudebr24@gmail.com>
2026-02-11 21:21:07 +0200

    query model

--- src/models.py
+++ src/models.py
@@ -1,8 +1,25 @@
 from pydantic import BaseModel, Field
-
+from typing import Literal, Union
 class Regulations(BaseModel):
     year: int = Field(2026, description="Year of FIA Regulations")
 
 class Race(BaseModel):
     year: int = Field(2026, description="Race Year.")
     gp_name: str = Field(..., description="Name of Country where Race is held.")
+
+class RouteQuery(BaseModel):
+    """
+    Route the user query to the most appropriate tool.
+    """
+    intent: Literal["REGULATIONS", "RACE_RESULTS", "GENERAL_CHAT"] = Field(
+        ..., 
+        description="The intent of the user. regulations=rules/specs, race_results=winners/podiums/standings, general_chat=other"
+    )
+    regulations_query: Union[Regulations, None] = Field(
+        None,
+        description="If intent is REGULATIONS, extract the year here."
+    )
+    race_query: Union[Race, None] = Field(
+        None,
+        description="If intent is RACE_RESULTS, extract year and gp_name here."
+    )
