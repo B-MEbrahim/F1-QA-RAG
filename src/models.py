0c20106fa7215ec82f7da7e0ab20b0a191a749db
B-MEbrahim <mahmoudebr24@gmail.com>
2026-02-11 20:59:34 +0200

    unused code

--- src/models.py
+++ src/models.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel, Field
-from typing import Any, Dict, Union, Optional
 
 class Regulations(BaseModel):
     year: int = Field(2026, description="Year of FIA Regulations")
