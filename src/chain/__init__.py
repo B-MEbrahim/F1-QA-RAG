e7f4fbf31200226ed8db17df70db49759f3bfb76
Mahmoud Ebrahim <142084714+B-MEbrahim@users.noreply.github.com>
2026-02-18 19:22:24 +0000

    init

--- src/chain/__init__.py
+++ src/chain/__init__.py
@@ -1,4 +1,4 @@
 from .chat_history import (get_chat_history, 
                            add_to_history,
                            clear_history)
-from.chain import get_answer, chat
+from.chain import get_answer, chat, answer_chain
